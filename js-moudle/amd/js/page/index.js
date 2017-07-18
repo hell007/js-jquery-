15449d5e1927c6cea48db0c9118687f52a981867
hell007 <1048523306@qq.com>
2017-07-18 15:27:59 +0800

    Update index.js

--- js-moudle/amd/js/page/index.js
+++ js-moudle/amd/js/page/index.js
@@ -1,16 +1,17 @@
 require(['app','utils','../index/template','../mod/fun'], function (app,Utils,template,fun){
 	//console.log(app)
 	
+	//工具类调用
 	var utils = new Utils();
 	console.log("空:"+ utils.isEmpty("dasdfs"));
 	
 	//app支持调用
 	app.handleOpenMask();
 	
-	//模板调用（underscore.js插件使用）
+	//js数据渲染 模板调用（underscore.js插件使用）
 	template.init();
 	
-	// jquery插件调用
+	//js交互
 	fun.init("#container");
 	
 });
