70ae9f9458509308211376ff2aadf436519d5bb4
hell007 <1048523306@qq.com>
2017-07-18 14:06:35 +0800

    Update config.js

--- js-moudle/amd/js/config.js
+++ js-moudle/amd/js/config.js
@@ -2,13 +2,19 @@ var requirejs = {
     //baseUrl: "js",
 	paths: {
 	    "jquery": "../lib/jquery-2.1.4",
+	    "art-template': '../lib/template-web",
+	    "juicer': '../lib/juicer-min",
 	    "underscore": "../lib/underscore",
 	    "app": "../lib/app",
+	    "utils": "../lib/utils",
 	    "imagesLoaded": "../lib/imagesloaded.pkgd"
 	},
 	shim: {
 	    'underscore':{
 			exports: '_'
+		},
+		'juicer':{
+			exports: 'juicer'
 		}
 	}
 };
