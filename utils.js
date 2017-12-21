49a290994cb1c7df339d37dccd9578bc512601ab
hell007 <1048523306@qq.com>
2017-12-21 17:29:36 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -17,3 +17,30 @@ console.log('1==',JSON.parse(JSON.stringify(isTouchDevice))[1])
 console.log('index==',isTouchDevice.index)
 
 console.log('input==',isTouchDevice.input)
+
+
+function support3d() {
+      var el = document.createElement('p'),
+          has3d,
+          transforms = {
+              'webkitTransform':'-webkit-transform',
+              'OTransform':'-o-transform',
+              'msTransform':'-ms-transform',
+              'MozTransform':'-moz-transform',
+              'transform':'transform'
+          };
+
+      // Add it to the body to get the computed style.
+      document.body.insertBefore(el, null);
+
+      for (var t in transforms) {
+          if (el.style[t] !== undefined) {
+              el.style[t] = 'translate3d(1px,1px,1px)';
+              has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
+          }
+      }
+
+      document.body.removeChild(el);
+
+      return (has3d !== undefined && has3d.length > 0 && has3d !== 'none');
+  }
