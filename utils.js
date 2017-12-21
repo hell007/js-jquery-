2358fae231eaa9a3a63e1a27a29752260756f238
hell007 <1048523306@qq.com>
2017-12-21 17:02:18 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -5,3 +5,15 @@ function showError(type, text){
 
 //匹配设备类型
 var isTouchDevice = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|playbook|silk|BlackBerry|BB10|Windows Phone|Tizen|Bada|webOS|IEMobile|Opera Mini)/);
+
+showError('info',isTouchDevice);
+
+console.log('isTouchDevice==',isTouchDevice)
+
+console.log('0==',JSON.parse(JSON.stringify(isTouchDevice))[0])
+
+console.log('1==',JSON.parse(JSON.stringify(isTouchDevice))[1])
+
+console.log('index==',isTouchDevice.index)
+
+console.log('input==',isTouchDevice.input)
